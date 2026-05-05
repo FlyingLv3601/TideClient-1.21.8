87763173d08493293ec31540b83e44c458f03994
lv3601 <onlylv3601@gmail.com>
2026-05-05 10:52:57 +0300

    Add TODO comment for settings in CONFIG.java
    Added a TODO comment for future settings implementation.

--- src/main/java/com/tideclient/Module/Config/CONFIG.java
+++ src/main/java/com/tideclient/Module/Config/CONFIG.java
@@ -12,6 +12,9 @@ import java.util.List;
 import com.tideclient.Module.Module;
 import java.util.Map;
 
+
+//TODO: add settings
+
 public class CONFIG {
     private static final String FILE = "config.json";
     private static final Gson gson = new Gson();
