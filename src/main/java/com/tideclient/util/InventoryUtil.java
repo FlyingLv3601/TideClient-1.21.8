4c63042174ba4ee5084d25061cfcf3e62e37fa5f
FlyingLv3601 <onlylv3601@gmail.com>
2026-05-13 22:34:34 +0300

    Created Inv util to move items in inv

---
+++ src/main/java/com/tideclient/util/InventoryUtil.java
@@ -0,0 +1,14 @@
+package com.tideclient.util;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.screen.slot.SlotActionType;
+
+public class InventoryUtil {
+    public static MinecraftClient mc = MinecraftClient.getInstance();
+    public static void moveTotem(int slot, int HandSlot) {
+        int syncId = mc.player.currentScreenHandler.syncId;
+        mc.interactionManager.clickSlot(syncId, slot, 0, SlotActionType.PICKUP, mc.player);
+        mc.interactionManager.clickSlot(syncId, HandSlot, 0, SlotActionType.PICKUP, mc.player);
+        mc.interactionManager.clickSlot(syncId, slot, 0, SlotActionType.PICKUP, mc.player);
+    }
+}
