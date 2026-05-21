e783fe9f3707b9bac9fd95b256646ce6142c447f
FlyingLv3601 <onlylv3601@gmail.com>
2026-05-21 21:16:00 +0300

    just code improve

--- src/main/java/com/tideclient/util/TextRenderUtil.java
+++ src/main/java/com/tideclient/util/TextRenderUtil.java
@@ -5,14 +5,13 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 
 public class TextRenderUtil {
+    private static MinecraftClient mc = MinecraftClient.getInstance();
     public static void TextRender(DrawContext ctx, String text, int x, int y){
-        MinecraftClient client = MinecraftClient.getInstance();
-        ctx.drawText(client.textRenderer, text, x, y, Color.activeBtn, false);
+        ctx.drawText(mc.textRenderer, text, x, y, Color.activeBtn, false);
     }
 
     public static void TextRender(DrawContext ctx, String text, int x, int y, int color){
-        MinecraftClient client = MinecraftClient.getInstance();
-        ctx.drawText(client.textRenderer, text, x, y, color, false);
+        ctx.drawText(mc.textRenderer, text, x, y, color, false);
     }
 
 }
